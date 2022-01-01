c7d25b22feff973c0405393981a4bc99f2d1e00f
Mark Aldrich <mark@mwaldrich.io>
2021-12-31 23:26:50 -0500

    add min max algo

--- src/backend/ai.js
+++ src/backend/ai.js
@@ -61,13 +61,22 @@ export function allKeepMasks() {
     return keepMasks;
 }
 
-class Backend {
+
+/**
+ * Returns a FRESH new roll.
+ * @returns Roll
+ */
+function freshRoll() {
+    return reroll([1, 1, 1, 1, 1], [false, false, false, false, false]);
+}
+
+export class Backend {
     constructor() {
         this.turnNumber = 1;
         this.rollsRemaining = 3;
 
         // The initial roll
-        this.roll = reroll([1, 1, 1, 1, 1], [false, false, false, false, false]);
+        this.roll = [1, 1, 1, 1, 1];
 
         // Empty scorecard
         this.scorecard = [null, null, null, null, null, null, null, null, null, null, null, null, null];
@@ -81,6 +90,145 @@ class Backend {
         // - goalRoll
         // - goalPlay
         // - actualPlay
+
+        let currentPlay, goalRoll, goalPlay, actualPlay;
+
+        // If we are out of rolls, the turn is over!
+        if (this.rollsRemaining == 0) {
+            currentPlay = score(this.roll, this.scorecard);
+            goalRoll = this.roll;
+            goalPlay = currentPlay;
+            actualPlay = currentPlay;
+
+            // process the next turn
+            this.processNextTurn();
+        } else {
+            // If there was a previous play calculated, perform it.
+            // Then get to work on the next one.
+            if (this.calculatedPlay) {
+                this.roll = reroll(this.roll, this.calculatedPlay);
+            }
+
+            // Find the next play
+            let [keep, goalRoll, goalPlay] = minmax();
+            currentPlay = score(this.roll, this.scorecard);
+            actualPlay = null;
+        }
+
+        return [
+            this.scorecard,
+            this.roll,
+            currentPlay,
+            goalRoll,
+            goalPlay,
+            actualPlay
+        ];
+    }
+
+    /**
+     * Performs 1-level min-max on the current yahtzee state.
+     * This means analyze the different ways to reroll, and select
+     * the one that gives you:
+     * 1. the highest minimum
+     * 2. the lowest maximum
+     * 
+     * @returns keep mask, goal roll, and goal play
+     */
+    minmax() {
+        // keepmask -> [[score, roll]]
+        let plays = {};
+
+
+        // Iterate through all keep masks
+        for (keep in allKeepMasks()) {
+            plays[keep] = [];
+
+            // Iterate through changing every die value.
+            // Calculate the minimum and maximums inside.
+            for (let first = 1; first < 6; first++) {
+                for (let second = 1; second < 6; second++) {
+                    for (let third = 1; third < 6; third++) {
+                        for (let fourth = 1; fourth < 6; fourth++) {
+                            for (let fifth = 1; fifth < 6; fifth++) {
+                                // reroll
+                                let r = [
+                                    keep[0]? this.roll[0] : first,
+                                    keep[1]? this.roll[1] : second,
+                                    keep[2]? this.roll[2] : third,
+                                    keep[3]? this.roll[3] : fourth,
+                                    keep[4]? this.roll[4] : fifth,
+                                ];
+
+                                // score
+                                let s = score(r, this.scorecard);
+
+                                // add to plays
+                                plays[keep].push([s, r])
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        // keepmask -> [[score, roll]]
+        // let plays = {};
+
+        // [[keepmask, min, max, goalRoll]]
+        let sortedKeeps = [];
+
+        // Figure out minimum and maximum scores for each play
+        for (let keep in allKeepMasks()) {
+            let min = Number.MAX_SAFE_INTEGER;
+            let max = Number.MIN_SAFE_INTEGER;
+            let goalRoll;
+
+            for (let play in plays[keep]) {
+                if (play[0] < min) {
+                    min = play[0];
+                }
+
+                if (play[0] > max) {
+                    max = play[0];
+                    goalRoll = play[1];
+                }
+            }
+
+            sortedKeeps.push([keep, min, max, goalRoll]);
+        }
+
+        // Find the play with the highest minimum and the lowest maximum
+        sortedKeeps
+            .sort(([a, min1, max1, c], [d, min2, max2, f]) => {
+                /** This is where the magic happens */
+                let minDiff = min2 - min1;
+                if (minDiff !== 0) {
+                    return minDiff;
+                } else {
+                    return max2 - max1;
+                }
+            });
+
+        // SELECT THE PLAY
+        let finalPlay = sortedKeeps[0];
+
+        return [finalPlay[0], finalPlay[3], score(finalPlay[3], this.scorecard)];
+    }
+
+    /**
+     * Processes a NEW TURN. This means when you are out of rolls and commit
+     * your play to the scorecard.
+     */
+    processNextTurn() {
+            // Commit play to scorecard.
+            let play = score(this.roll, this.scorecard);
+            this.scorecard[play[0]] = play[1];
+
+            // Deal with other state
+            this.turnNumber++;
+            this.roll = freshRoll();
+            this.rollsRemaining = 3;
     }
 
 }
